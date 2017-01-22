c35818bbd73b2b2e437aae8068d95d588268907e
Ally Weir <ally.w@simul8.com>
2017-01-22 16:03:31 +0000

    Add constructor to Player class

--- Battleships/Player.cs
+++ Battleships/Player.cs
@@ -10,6 +10,18 @@ namespace Battleships
     {
         #region Properties
 
+        public string Name { get; }
+        public Board GameBoard { get; }
+
+        #endregion
+
+        #region Constructors
+
+        public Player(string name)
+        {
+            Name = name;
+            GameBoard = new Board();
+        }
 
         #endregion
     }
